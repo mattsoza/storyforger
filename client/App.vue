45af4970a2cebacd3a79eeec3dbecc47655c7d0d
mattsoza <mattsoza@protonmail.com>
2022-11-22 18:10:02 -0500

    Setup basic main and app files

---
+++ client/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <header>
+      <NavBar />
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/common/NavBar.vue';
+
+export default {
+  name: 'App',
+  components: {NavBar},
+  beforeCreate() {
+    fetch('/api/users/session', {
+      credentials: 'same-origin'  // sends express-session credentials with request
+    }).then(res => res.json()).then(res => {
+      const user = res.user;
+      this.$store.commit('setUsername', user ? user.username : null);
+    });
+  }
+}
+
+</script>
