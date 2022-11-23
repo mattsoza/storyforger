20b89c4708633a25028d55109a8f7fc7563d92a1
mattsoza <mattsoza@protonmail.com>
2022-11-23 01:23:50 -0500

    Basic login elements displayed on front-page

--- client/components/Login/LoginPage.vue
+++ client/components/Login/LoginPage.vue
@@ -8,8 +8,10 @@
     <h2>
       Make choose-your-own-adventure books with ease!
     </h2>
-    <button id="signin">Sign-in</button>
-    <button id="login">Login</button>
+    <div id="buttons">
+      <button class="acct signup">Sign-in</button>
+      <button class="acct login">Login</button>
+    </div>
   </div>
 </template>
 
@@ -22,3 +24,42 @@ export default {
   components: {LoginForm, SignupForm}
 };
 </script>
+
+<style lang="scss">
+
+  h1 {
+    font-size: 4em;
+    margin-bottom: 0rem;
+  }
+
+  #frontPage {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #buttons {
+    display: flex;
+    width: 30%;
+    justify-content: space-around;
+  }
+
+  .acct {
+    font-size: 1.5em;
+    padding: .4rem .6rem .4rem .6rem;
+    border-radius: 10px;
+
+    &.signup {
+      background-color: #4DBCEC;
+      color: white;
+    }
+
+    &.signup:hover {
+      background-color: #3D95BA;
+    }
+
+    &.login {
+      
+    }
+  }
+</style>
--- client/router.ts
+++ client/router.ts
@@ -3,39 +3,40 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import LoginPage from './components/Login/LoginPage.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
-    {path: '/', name: 'Home', component: homePage},
-    {path: '/my-stories', name: 'MyStories', component: StoryHome},
-    {path: '/account', name: 'Account', component: AccountPage},
-    {path: '/login', name: 'Login', component: LoginPage}
+    {path: '/', name: 'Home', component: LoginPage}
+    // {path: '/my-stories', name: 'MyStories', component: StoryHome},
+    // {path: '/account', name: 'Account', component: AccountPage}
 ]
 
-const router = new VueRouter({routes})
+const router = new VueRouter({routes});
 
 /**
  * Navigation guards to prevent user from accessing wrong pages.
  */
- router.beforeEach((to, from, next) => {
-    if (router.app.$store) {
-      if (to.name === 'Login' && router.app.$store.state.username) {
-        next({name: 'Account'}); // Go to Account page if user navigates to Login and are signed in
-        return;
-      }
-
-      if (to.name === 'Home' && router.app.$store.state.username) {
-        next({name: 'MyStories'});
-        return;
-      }
-  
-      if (to.name === 'Account' && !router.app.$store.state.username) {
-        next({name: 'Login'}); // Go to Login page if user navigates to Account and are not signed in
-        return;
-      }
+router.beforeEach((to, from, next) => {
+  if (router.app.$store) {
+    if (to.name === 'Login' && router.app.$store.state.username) {
+      next({name: 'Account'}); // Go to Account page if user navigates to Login and are signed in
+      return;
     }
+
+    if (to.name === 'Home' && router.app.$store.state.username) {
+      next({name: 'MyStories'});
+      return;
+    }
+
+    if (to.name === 'Account' && !router.app.$store.state.username) {
+      next({name: 'Login'}); // Go to Login page if user navigates to Account and are not signed in
+      return;
+    }
+  }
+
+  next();
+});
   
-    next();
-  });
-  
-  export default router;
+export default router;
