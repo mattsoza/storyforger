2b0ca9e986203b38b3f1a5683ae55e10080603dd
Ash <acasden@mit.edu>
2022-12-13 17:00:40 -0500

    slighly more intuitive

--- client/components/Connection/ConnectionListComponent.vue
+++ client/components/Connection/ConnectionListComponent.vue
@@ -5,7 +5,9 @@
       <ul>
         <!-- Our connections are: {{findPageConnections()}}  -->
         <!-- {{connections}}. -->
-        <button @click="findPageConnections">Click here to findPageConnections</button>
+        Travel from this Page to any other pages listed below!
+
+        <!-- <button @click="findPageConnections">Click here to findPageConnections</button> -->
         <div v-if="(pages.length!==0 && pages.length!==1)">
 
         <li
--- client/components/Connection/NewConnectionForm.vue
+++ client/components/Connection/NewConnectionForm.vue
@@ -34,8 +34,9 @@ export default {
   methods: {
     async createConnection () {
       // const book = this.$store.state.currentBook ?? '';
-      console.log('our params', this.page._id, 'two', this.child._id, 'thiree', this.text, 'done')
-      const fields = { parent: this.page._id, child: this.child._id, text: this.text }
+      // console.log('our params', this.page._id, 'two', this.child._id, 'thiree', this.text, 'done')
+      const text = "Jump To " + this.child.title;
+      const fields = { parent: this.page._id, child: this.child._id, text: text }
       const response = await fetch(`/api/connection/`, { method: 'POST', body: JSON.stringify(fields), headers: { 'Content-Type': 'application/json' } })
       console.log(response)
 
